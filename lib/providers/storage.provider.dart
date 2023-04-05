1c827ac14d0baaba4c2b4c0d209cac2f5076e0bb
ajay <ajaynairarun@gmail.com>
2023-04-05 20:20:37 +0530

    remove : new keyword from class creation

--- lib/providers/storage.provider.dart
+++ lib/providers/storage.provider.dart
@@ -3,6 +3,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter/material.dart';
 
 class StorageData extends ChangeNotifier {
-  final FlutterSecureStorage _storage = new FlutterSecureStorage();
+  final FlutterSecureStorage _storage = FlutterSecureStorage();
   FlutterSecureStorage get storage => _storage;
 }
